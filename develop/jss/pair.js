7fca08e293020f4b9c351224def72031afaebeb5
rxv03545 <rxv03545@gmail.com>
2025-09-02 00:55:17 +0900

    playerクラスまで実装した

--- develop/jss/pair.js
+++ develop/jss/pair.js
@@ -164,5 +164,167 @@ static isThreeCard = (cards) => {
 
     return isPair;
 };
+
+
+/**ツーペアの成否判定    */
+static isTwoPair = (cards) => {
+    let isPair = false;
+    if (
+        cards[0].rank === cards[1].rank &&
+        cards[2].rank === cards[3].rank &&
+        cards[0].rank !== cards[2].rank
+    ) {
+        isPair = true;
+
+        this.#rank = Util.sum(cards[0].rank, cards[1].rank, cards[2].rank, cards[3].rank);
+    }
+
+    else if (
+        cards[0].rank === cards[1].rank &&
+        cards[3].rank === cards[4].rank &&
+        cards[0].rank !== cards[3].rank
+    ) {
+        isPair = true;
+
+        this.#rank = Util.sum(cards[0].rank, cards[1].rank, cards[3].rank, cards[4].rank);
+    }
+
+    else if (
+        cards[2].rank === cards[3].rank &&
+        cards[2].rank === cards[4].rank
+    ) {
+        isPair = true;
+
+        this.#rank = Util.sum(cards[2].rank, cards[3].rank, cards[4].rank);
+    }
+
+    return isPair;
+};
+
+
+/**ワンペアの成否判定 */
+static isOnePair = (cards) => {
+    let isPair = false;
+    if (
+        cards[0].rank === cards[1].rank &&
+        cards[0].rank !== cards[2].rank &&
+        cards[0].rank !== cards[3].rank &&
+        cards[0].rank !== cards[4].rank
+    ) {
+        isPair = true;
+
+        this.#rank = Util.sum(cards[0].rank, cards[1].rank);
+    }
+
+    else if (
+        cards[1].rank === cards[2].rank &&
+        cards[1].rank !== cards[0].rank &&
+        cards[1].rank !== cards[3].rank &&
+        cards[1].rank !== cards[4].rank
+    ) {
+        isPair = true;
+
+        this.#rank = Util.sum(cards[1].rank, cards[2].rank);
+    }
+
+    else if (
+        cards[2].rank === cards[3].rank &&
+        cards[2].rank !== cards[0].rank &&
+        cards[2].rank !== cards[1].rank &&
+        cards[2].rank !== cards[4].rank
+    ) {
+        isPair = true;
+
+        this.#rank = Util.sum(cards[2].rank, cards[3].rank);
+    }
+
+    else if (
+        cards[3].rank === cards[4].rank &&
+        cards[3].rank !== cards[0].rank &&
+        cards[3].rank !== cards[1].rank &&
+        cards[3].rank !== cards[2].rank
+    ) {
+        isPair = true;
+
+        this.#rank = Util.sum(cards[3].rank, cards[4].rank);
+    }
+
+    return isPair;
+};
+
+
+/** 成立条件を満たす最も強い役を判定する     */
+static judge = (cards) => {
+    let result = null;
+
+//カード配列のコピーを作成する
+    const _cards = [...cards];
+    //ランクが小さい順にカードをソートする
+    _cards.sort((a,b) => a.rank - b.rank);
+
+
+    /**役が強い順に判定する  */
+
+    if (this.isRoyalStraightFlush(_cards)) {
+        result ={
+            strength:9,rank:this.rank,hand:"ロイヤルストレートフラッシュ",
+        };
+    }
+
+    if (this.isStraightFlush(_cards)) {
+        result ={
+            strength:8,rank:this.rank,hand:"ストレートフラッシュ",
+        };
+    }
+
+    if (this.isFourCard(_cards)) {
+        result ={
+            strength:7,rank:this.rank,hand:"フォーカード",
+        };
+    }
+
+    if (this.isFullHouse(_cards)) {
+        result ={
+            strength:6,rank:this.rank,hand:"フルハウス",
+        };
+    }
+
+    if (this.isFlush(_cards)) {
+        result ={
+            strength:5,rank:this.rank,hand:"フラッシュ",
+        };
+    }
+
+    if (this.isStraight(_cards)) {
+        result ={
+            strength:4,rank:this.rank,hand:"ストレート",
+        };
+    }
+
+    if (this.isThreeCard(_cards)) {
+        result ={
+            strength:3,rank:this.rank,hand:"スリーカード",
+        };
+    }
+
+    if (this.isTwoPair(_cards)) {
+        result ={
+            strength:2,rank:this.rank,hand:"ツーペア",
+        };
+    }
+
+    if (this.isOnePair(_cards)) {
+        result ={
+            strength:1,rank:this.rank,hand:"ワンペア",
+        };
+    }
+//役が成立していない場合 */
+else {
+    result ={
+        strength:0,rank:0,hand:"ノーペア",
+    };
 }
 
+
+    return result;
+};
---
+++ develop/jss/player.js
@@ -0,0 +1,80 @@
+import Card from "./card.js";
+
+/playerクラス/
+export default class Player {
+/*プロパティ
+#cards;
+#nodes;
+
+/**プレイヤーの手札
+     */
+    get cards() {
+        return this.#cards;
+    }
+
+    /**手札のノード（リスト） */
+    get nodes() {
+        return this.#nodes;
+    }
+
+
+//選択しているノード（リスト）
+
+get selectedNodes(){
+//nodesの中から条件を満たす要素だけ集めた新しい配列
+return this.nodes.filter((node) =>
+    node.classList.contains("selected"));
+}
+
+/**コンストラクタ    */
+constructor(selector){
+    this.#nodes = Array.from(document.querySelectorAll(selector));
+    this.#cards = [];
+}
+
+/**手札を描画する */
+displayCard(front){
+    this.cards.forEach((card,index) => {
+        let name = String(card.index).padStart(2,"0")+".png";
+//裏面を表示する場合は画像を変更
+        if (!front) {
+            name = "red.png";
+        }
+        //カードの画像をセット
+        this.nodes[index].setAttribute("src","images/"+name);
+    });
+};
+
+/*新しいカードを手札に追加する*/
+addCard(newCard){
+    this.cards.push(newCard);
+    //最後尾のノードにカードのインデックス番号を書き込む    
+    this.nodes[this.cards.length-1].dataset.index = newCard.index;
+};
+
+/*交換するカードを選択する*/
+selectCard(node){
+    //選択状態を表すCSSクラス名を切り替える
+    node.classList.toggle("selected");
+};
+
+/*山札からカードを引いて交換する*/
+drawCard(newCard){
+    //選択しているノードを先頭から一つ取り出す
+    const node = this.selectedNodes.shift();
+    //このノードに書かれたインデックス番号を取得する
+    const index = parseInt(node.dataset.index);
+    //このノードに書かれた手札の位置を検索する
+    const pos = this.cards.findIndex(card) => card.index === index);
+    //この手札を複製して退避しておく
+    const oldCard = this.cards.slice(pos,pos+1)[0];
+    //この手札を新しいカードで置き換える
+    this.cards[pos] = newCard;
+    //このノードに新しいカードのインデックス番号を書き込む
+    node.dataset.index = newCards.index;
+    //このノードを未選択の状態に戻す
+    node.classList.remove("selected");
+    //退避したカードを返す
+    return oldCard;
+};
+}
